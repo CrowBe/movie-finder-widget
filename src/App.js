16a1c92b3378997b6f2e48c5878a2c98b56efc0f
CrowBe <benny_crow91@hotmail.com>
2020-05-04 15:08:08 +0900

    Basic api call and results mapping

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [ collection, setCollection ] = useState([]);
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const results = collection.map((item) => <h3>{item.title}</h3>);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            const res = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`);
+            res.json()
+                .then((data) => setCollection(data.results))
+                .catch((error) => console.log(error));
+        };
+
+        fetchResults();
+    })
+
+    return (
+        <div className="App">
+            <header>
+                {/* {Set up search bar} */}
+            </header>
+            {/* Create a component and map the collection to each component.*/}
+            <div>{results}</div>
+        </div>
+    );
 }
 
 export default App;
