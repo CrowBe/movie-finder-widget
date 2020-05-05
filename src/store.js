7d6d800505e7b83e4f31feda526cd1885e4ea7f5
CrowBe <benny_crow91@hotmail.com>
2020-05-05 16:24:53 +0900

    managing state locally with react hooks

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import reducers from './reducers';
-
-// Create store by passing in combined reducers and enable redux devtools.
-export default createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
