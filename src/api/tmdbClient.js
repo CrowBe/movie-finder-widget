4c4a5a92432bdd2bd4c82dacd7cf1551ea606e5c
CrowBe <benny_crow91@hotmail.com>
2020-05-23 14:35:25 +0900

    Updated package.json file to fix dependancy issues

--- src/api/tmdbClient.js
+++ src/api/tmdbClient.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// Necessary for heroku according to stack overflow
+require('dotenv').config()
+
 // retrieve the api key from the env variables
 const apiKey = process.env.REACT_APP_API_KEY;
 
