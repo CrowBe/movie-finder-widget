43dc67b636c3e0993f18341e0c4f42da0c8955da
Ben Crow <48108358+CrowBe@users.noreply.github.com>
2023-09-19 22:22:01 +1000

    moving to ts friendly ellipsis package

--- src/components/card_components/CardText.tsx
+++ src/components/card_components/CardText.tsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ItemContext } from '../../context';
 import tmdbClient from '../../api/tmdbClient';
-import { Ellipsis } from 'ftellipsis';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 const CardText = () => {
     const item = useContext(ItemContext);
@@ -14,24 +14,17 @@ const CardText = () => {
                 .then(response => setText(response.data.biography))
                 .catch(error => console.log(error));
         };
-        // npm package from https://github.com/ftlabs/ftellipsis that works with css to
-        // cut off the text at the end of the container and place an ellipsis.
-        const element = document.getElementById(`${item.id}text`);
-        const ellipsis = new Ellipsis(element.firstChild);
-        // Works out the target height
-        ellipsis.calc();
-        // Cuts off the text and sets an ellipsis in the correct location dynamically
-        ellipsis.set();
-                // The dependencies below trigger a warning, but following the suggestion causes
-        // looping. See https://medium.com/@andrewmyint/infinite-loop-inside-useeffect-react-hooks-6748de62871
     }, [text]);
 
     return (
-        <div className="card-text-div" id={`${item.id}text`} >
-            <div>
-                <p>{text || "No Information Available."}</p>
-            </div>
-        </div>
+        <LinesEllipsis
+            className='card-text-div'
+            text={text || "No Information Available."}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+        />
     )
 }
 
